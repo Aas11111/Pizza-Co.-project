c3c342d0bc34f9b58ca87f4d6e9310256c8bb931
Aas11 <aas111.hope@gmail.com>
2024-01-02 00:41:26 +0530

    changes

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 //https://vitejs.dev/config/
 export default defineConfig({
-  // base: "/fast-react-pizza-project/",
-  //
+  base: "/fast-react-pizza-project/",
+
   plugins: [react()],
 });
