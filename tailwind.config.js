b4ead3fa69d05bb75b2506e5bdc0530fabe5fbea
Aas11 <aas111.hope@gmail.com>
2024-01-02 15:40:49 +0530

    changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,22 @@ export default {
       height: {
         screen: "100dvh",
       },
+
+      screens: {
+        xs: "600px",
+
+        // sm: "640px",
+        // => @media (min-width: 640px) { ... }
+
+        // md: "768px",
+        // // => @media (min-width: 768px) { ... }
+
+        // lg: "1024px",
+        // // => @media (min-width: 1024px) { ... }
+
+        // xl: "1280px",
+        // // => @media (min-width: 1280px) { ... }
+      },
     },
   },
   plugins: [],
